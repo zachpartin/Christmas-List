d166acf9d651db79af4747c34cca832b7a6978a2
Zach Partin <zachpartin@zachs-mbp.myfiosgateway.com>
2021-10-27 14:15:59 -0400

    added buttons

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,10 +1,33 @@
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
     <div>
       <h1>Virtual Christmas List</h1>
       <h4>A Christmas List for the 21st Century</h4>
+
+      <div className="buttondiv">
+        <Link to="/electronics">
+        <button>Electronics</button>
+        </Link>
+
+        <Link>
+        <button>Toys</button>
+        </Link>
+
+        <Link>
+        <button>Sporting Goods</button>
+        </Link>
+
+        <Link>
+        <button>Clothing</button>
+        </Link>
+
+        <Link>
+        <button>Other</button>
+        </Link>
+      </div>
     </div>
   );
 };
